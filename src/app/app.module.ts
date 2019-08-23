96df14371423f0949fbb9ec876af3d6776ba2ed0
chris <christianwilliamsjire@gmail.com>
2019-08-23 15:02:56 +0300

    this commit will add search-form to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
 import { NavComponent } from './nav/nav.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubComponent,
-    NavComponent
+    NavComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,

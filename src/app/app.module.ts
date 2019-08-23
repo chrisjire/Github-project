ccde71ce3b574e44444c03f6709ee0dbce4b6655
chris <christianwilliamsjire@gmail.com>
2019-08-23 12:40:53 +0300

    this commit will add nav bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubComponent } from './github/github.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubComponent
+    GithubComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
